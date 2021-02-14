e39d0ee1782703b6395661f6dd6e79ccdde8714d
pozys <pozys@mail.ru>
2021-02-14 14:40:06 +0300

    google update

--- Lesson_7/config/googleAuth.js
+++ Lesson_7/config/googleAuth.js
@@ -1,7 +1,7 @@
 const config = {
   ClientID:
-    "711389892977-eqgim161lk0t56lfiqdtoc2l97b13umk.apps.googleusercontent.com",
-  ClientSecret: "_fDJJ9tdRAtoU7rQDN6-j-wB",
+    "...",
+  ClientSecret: "...",
 };
 
 module.exports = config;

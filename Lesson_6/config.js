4562fc7f4aff5172572d51267612e49972b46502
pozys <pozys@mail.ru>
2021-02-14 14:41:17 +0300

    google update

--- Lesson_6/config.js
+++ Lesson_6/config.js
@@ -3,8 +3,8 @@ const config = {
   user: "root",
   database: "HW5",
   password: "root",
-  googleClientID: "711389892977-eqgim161lk0t56lfiqdtoc2l97b13umk.apps.googleusercontent.com",
-  googleClientSecret: "_fDJJ9tdRAtoU7rQDN6-j-wB",
+  googleClientID: "...",
+  googleClientSecret: "...",
 };
 
 module.exports = config;
